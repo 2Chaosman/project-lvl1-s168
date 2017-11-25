f69a9066fbfd252f3176c7b0a1b034a2b1d062fa
Sergey Muhin <2chaosman@gmail.com>
2017-11-25 17:22:55 +0300

    add isprime game

---
+++ src/bin/brain-isprime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import launchGame from '../games/brain-isprime';
+
+launchGame();
---
+++ src/games/brain-isprime.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import {
+  getRandomNumber,
+} from '../utils';
+import startGame from '../index';
+
+const askQuestion = () => getRandomNumber(100);
+
+const checkForPrime = (num) => {
+  for (let i = num-1; i > 1; i -= 1) {
+    if (num % i === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+}
+
+const getGameData = () => {
+  const question = askQuestion();
+  const answer = checkForPrime(question);
+  return [question, answer];
+}
+
+export default () => {
+  const gameDescription = 'Is this number prime?';
+  return startGame(gameDescription, getGameData);
+}
